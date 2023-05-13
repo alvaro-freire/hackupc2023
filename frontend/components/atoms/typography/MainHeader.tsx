1ec8e16bed054054ae33719b45326d51c3e1ed00
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2023-05-13 02:40:58 +0200

    Modified: Main header

--- frontend/components/atoms/typography/MainHeader.tsx
+++ frontend/components/atoms/typography/MainHeader.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image';
 const MainHeader: React.FC = () => {
   return (
     <div className="flex flex-col justify-center my-8">
-      <h1 className="text-4xl text-center"> Vueling </h1>
-      <h2 className="text-center"> In air entertaiment </h2>
+      <div className="flex justify-center">
+        <Image src="/Logo_Vueling.svg" height={50} width={200} alt="logo_vueling" />
+      </div>
+      <h2 className="text-center text-xl"> in flight entertainment </h2>
     </div>
   )
 }
