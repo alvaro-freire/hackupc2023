3934bf0c070168fc6a63bc720dd7a4cd6fee538a
Álvaro Freire <alvaro.freirea@udc.es>
2023-05-13 15:44:46 +0200

    add transport endpoints

--- backend/controllers/transport/index.js
+++ backend/controllers/transport/index.js
@@ -1,5 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+
 module.exports = (app, authenticateJWT) => {
   app.get('/transport', authenticateJWT, (req, res) => {
-    res.json({ success: true })
+    const { dst } = req
+
+    const rawdata = fs.readFileSync(
+      path.join(__dirname, `locations/${dst}.json`)
+    )
+    const locations = JSON.parse(rawdata)
+
+    res.json(locations)
   })
+
+
+  app.post('/transport', authenticateJWT, (req, res) => {
+    const { dst, nickname } = req
+    const { location, method } = req.body
+
+
+  })
+
 }
