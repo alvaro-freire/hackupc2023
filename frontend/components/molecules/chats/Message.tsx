e59a88264ff30ec96c502567a716eedfad132db1
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2023-05-14 02:16:40 +0200

    Added: User chat indicator

--- frontend/components/molecules/chats/Message.tsx
+++ frontend/components/molecules/chats/Message.tsx
@@ -8,8 +8,9 @@ import useCredentials from '@/hooks/useCredentials';
 const leftBubble = (content: string, author: IAuthor): React.ReactElement => {
   if (!content) { return (<></>); }
   return (
-    <div className="w-full flex justify-start">
-      <div className="w-80 rounded-2xl rounded-tl-none p-3 bg-accent">
+    <div className="w-full flex flex-col justify-end w-80 rounded-2xl rounded-tl-none p-3 bg-accent">
+      <span className="text-sm"> {author.username}({author.seat})</span>
+      <div className="">
         {content}
       </div>
     </div>
@@ -19,8 +20,9 @@ const leftBubble = (content: string, author: IAuthor): React.ReactElement => {
 const rightBubble = (content: string, author: IAuthor): React.ReactElement => {
   if (!content) { return (<></>); }
   return (
-    <div className="w-full flex justify-end">
-      <div className="w-80 rounded-2xl rounded-tr-none p-3 bg-primary">
+    <div className="w-full flex flex-col justify-end w-80 rounded-2xl rounded-tr-none p-3 bg-primary">
+      <span className="text-sm"> {author.username}({author.seat})</span>
+      <div>
         {content}
       </div>
     </div>
